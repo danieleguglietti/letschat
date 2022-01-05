e8b9a3aba4daae7b71f4f48cebd1a138ac570879
danieleguglietti <danieleguglietti@gmail.com>
2022-01-05 13:03:01 +0100

    feat: added private modifier macro

--- include/letschat/request.h
+++ include/letschat/request.h
@@ -1,6 +1,9 @@
 #ifndef __REQUEST__
 #define __REQUEST__
 
-
+#ifndef __PRIVATE_MODIFIER
+#   define __PRIVATE_MODIFIER(x) __##x
+#   define private(x) __PRIVATE_MODIFIER(x)
+#endif
 
 #endif
--- include/letschat/response.h
+++ include/letschat/response.h
@@ -1,6 +1,9 @@
 #ifndef __RESPONSE__
 #define __RESPONSE__
 
-
+#ifndef __PRIVATE_MODIFIER
+#   define __PRIVATE_MODIFIER(x) __##x
+#   define private(x) __PRIVATE_MODIFIER(x)
+#endif
 
 #endif // __RESPONSE__
