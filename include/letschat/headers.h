309e2e77c0beb1fe8c77180c599379f2602b2fcf
danieleguglietti <danieleguglietti@gmail.com>
2022-01-05 22:22:41 +0100

    feat: added headers prototypes

---
+++ include/letschat/headers.h
@@ -0,0 +1,28 @@
+#ifndef __HEADERS__
+#define __HEADERS__
+
+#include "hashtable.h"
+
+typedef hashtable_t* headers_t;
+typedef entry_t* header_t;
+
+#define HEADERS_MAXCOUNT 32     // ? 32 chars = 32  B (256 bits)
+#define HEADER_FIELD_MAXLEN 32  // ? 32 chars = 32  B (256 bits)
+
+/**
+ * @brief Get a raw string of a headers list and parse into a header hashtable.
+ * 
+ * @param raw_headers The string to parse
+ * @return headers_t The parsed hashtable
+ */
+headers_t headers_parse(const char* raw_headers);
+
+/**
+ * @brief Get a headers hashtable and serialize it into a raw headers list string.
+ * 
+ * @param headers The hashtable to serialize
+ * @return char* The serialized headers list string
+ */
+char* headers_serialize(headers_t headers);
+
+#endif // __HEADERS__
