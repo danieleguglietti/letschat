2ee1300c8424e05fb7bd640f259b2b36463b5c0a
danieleguglietti <danieleguglietti@gmail.com>
2022-01-05 12:58:11 +0100

    feat: defined hash function, created the set and get function, implemented hashtable

--- include/letschat/hashtable.h
+++ include/letschat/hashtable.h
@@ -1,6 +1,49 @@
 #ifndef __HASHTABLE__
 #define __HASHTABLE__
 
+#include <stdint.h>
+#include <stdbool.h>
 
+#ifndef __PRIVATE_MODIFIER
+#   define __PRIVATE_MODIFIER(x) __##x
+#   define private(x) __PRIVATE_MODIFIER(x)
+#endif
+
+#define HASHTABLE_STARTLEN 16
+
+typedef struct entry
+{
+    const char* key;
+    char* value;
+} entry_t;
+
+typedef struct hashtable
+{
+    /* PUBLIC */
+    uint32_t size;
+
+    /* PRIVATES */
+    entry_t** private(entries);
+    uint32_t private(capacity);
+
+    /* METHODS */
+    entry_t* (*get)(struct hashtable* table, const char* key);
+    bool (*set)(struct hashtable* table, const char* key, char* value);
+} hashtable_t;
+
+
+/**
+ * @brief Create a new hashtable object.
+ * 
+ * @return hashtable_t* The new hashtable.
+ */
+hashtable_t* hashtable_new();
+
+/**
+ * @brief Deletes the hashtable and all its entries.
+ * 
+ * @param table The hashtable to delete.
+ */
+void hashtable_free(hashtable_t* table);
 
 #endif // __HASHTABLE__
--- src/hashtable.c
+++ src/hashtable.c
@@ -1,1 +1,143 @@
 #include "letschat/hashtable.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define FNV_OFFSET 14695981039346656037UL
+#define FNV_PRIME 1099511628211UL
+
+static uint64_t hash(const char* key)
+{
+    uint64_t hash = FNV_OFFSET;
+
+    for (const char* ch = key; *ch != '\0'; ch++)
+    {
+        hash ^= *ch;
+        hash *= FNV_PRIME;
+    }
+
+    return hash;
+}
+
+#define GET_INDEX(key, table) (hash(key) & (table->__capacity - 1))
+
+static entry_t* get(hashtable_t* table, const char* key)
+{
+    uint32_t index = (uint32_t) GET_INDEX(key, table);
+    entry_t** entries = table->__entries;
+
+    while (entries[index] != NULL)
+    {
+        entry_t* entry = entries[index];
+
+        if (strcmp(entry->key, key) == 0)
+            return entry;
+
+        index++;
+
+        if (index == table->__capacity)
+        {
+            index = 0;
+        }
+    }
+
+    return NULL;
+}
+
+static entry_t* entry_new(const char* key, char* value)
+{
+    entry_t* entry = malloc(sizeof *entry);
+
+    entry->key = key;
+    entry->value = value;
+
+    return entry;
+}
+
+static void set_entry(entry_t** entries, hashtable_t* table, const char* key, char* value)
+{
+    uint32_t index = (uint32_t) GET_INDEX(key, table);
+
+    while (entries[index] != NULL)
+    {
+        entry_t* entry = entries[index];
+
+        if (strcmp(entry->key, key) == 0)
+        {
+            entry->value = value;
+            return;
+        }
+
+        index++;
+
+        if (index == table->__capacity)
+        {
+            index = 0;
+        }
+    }
+
+    free(entries[index]);
+    entries[index] = entry_new(key, value);
+
+    table->size++;
+}
+
+static void resize(hashtable_t* table)
+{
+    uint32_t new_capacity = table->__capacity * 2;
+    entry_t** new_entries = calloc(new_capacity, sizeof *new_entries);
+
+    for (uint32_t i = 0; i < table->__capacity; i++)
+    {
+        entry_t* entry = table->__entries[i];
+
+        if (entry != NULL)
+            set_entry(new_entries, table, entry->key, entry->value);
+    }
+
+    free(table->__entries);
+    table->__entries = new_entries;
+
+    table->__capacity = new_capacity;
+}
+
+static bool set(hashtable_t* table, const char* key, char* value)
+{
+    if (value == NULL)
+        return false;
+
+    if (table->size == table->__capacity * 0.5)
+        resize(table);
+
+    set_entry(table->__entries, table, key, value);
+
+    return true;
+}
+
+hashtable_t* hashtable_new()
+{
+    hashtable_t* table = malloc(sizeof *table);
+
+    table->size = 0;
+    table->__capacity = HASHTABLE_STARTLEN;
+
+    table->__entries = calloc(table->__capacity, sizeof *table->__entries);
+
+    table->get = get;
+    table->set = set;
+
+    return table;
+}
+
+void hashtable_free(hashtable_t* table)
+{
+    for (uint32_t i = 0; i < table->__capacity; i++)
+    {
+        entry_t* entry = table->__entries[i];
+
+        if (entry != NULL)
+            free(entry);
+    }
+
+    free(table->__entries);
+    free(table);
+}
