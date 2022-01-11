4261af97af901e5904291f342fe38c96f0865116
danieleguglietti <danieleguglietti@gmail.com>
2022-01-11 12:31:32 +0100

    test: added headers serializer test

--- test/headers.cpp
+++ test/headers.cpp
@@ -2,7 +2,8 @@
 #include <gtest/gtest.h>
 #include <string>
 
-const char* raw = "Content-Type: application/json;Content-Length: 12;Connection: keep-alive;";
+const char* raw = "Content-Type:application/json;Connection:keep-alive;Content-Length:12;";
+constexpr int max_length = (HEADER_FIELD_MAXLEN * 2 + 2) * HEADERS_MAXCOUNT;
 
 TEST(Headers, Parser) {
     headers_t headers = headers_parse(raw);
@@ -10,4 +11,14 @@ TEST(Headers, Parser) {
     ASSERT_NE(headers, nullptr);
     EXPECT_STREQ(headers->get(headers, "Content-Type")->value, "application/json");
     EXPECT_EQ(headers->size, 3);
+}
+
+TEST(Headers, Serializer) {
+    headers_t headers = headers_parse(raw);
+
+    ASSERT_NE(headers, nullptr);
+    std::string serialized = headers_serialize(headers);
+    EXPECT_LE(serialized.size(), max_length);
+    EXPECT_EQ(serialized.size(), strlen(raw));
+    EXPECT_STREQ(serialized.c_str(), raw);
 }
