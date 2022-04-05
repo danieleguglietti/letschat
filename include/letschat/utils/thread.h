432b61bdbc02bc937953be472697d17a52a06a8f
Daniele Guglietti <gugliettidaniele@gmail.com>
2022-04-05 21:46:09 +0200

    feat: started thread support

--- include/letschat/utils/thread.h
+++ include/letschat/utils/thread.h
@@ -1,20 +1,24 @@
 #ifndef __THREAD__
 #define __THREAD__
 
-#include <stdint.h>
-
 #ifdef _WIN32
 #   include <windows.h>
+#   define ROUTINE_RETV DWORD
+#   define ROUTINE_ARGS LPVOID
 #else
 #   include <pthread.h>
 #   include <unistd.h>
+#   define ROUTINE_RETV void*
+#   define ROUTINE_ARGS void*
+#   define __stdcall
 #endif
+
+#define ROUTINE(name) ROUTINE_RETV __stdcall name(ROUTINE_ARGS args)
+typedef ROUTINE_RETV (__stdcall *routine_t)(ROUTINE_ARGS);
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-
 
 #ifdef __cplusplus
 }
